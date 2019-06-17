dfe02bdffd80cff0d64d4146a0039b33d651ce87
Julian Vila <vilaj46@gmail.com>
2019-06-17 14:17:24 -0400

    Updates

---
+++ precache-manifest.003ab31b53fde3b632af6a5ec0ab78cc.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87ab5ff17c2354c873ac101169ec5846",
+    "url": "/mock-critical/index.html"
+  },
+  {
+    "revision": "60da85b63c47dd84c2ae",
+    "url": "/mock-critical/static/css/main.0592e02a.chunk.css"
+  },
+  {
+    "revision": "21a8360c2a1bdd359d77",
+    "url": "/mock-critical/static/js/2.98690753.chunk.js"
+  },
+  {
+    "revision": "60da85b63c47dd84c2ae",
+    "url": "/mock-critical/static/js/main.45985c6b.chunk.js"
+  },
+  {
+    "revision": "0d11969069b479df7522",
+    "url": "/mock-critical/static/js/runtime~main.e20727b1.js"
+  },
+  {
+    "revision": "b2ad852e2e03bd0c90737b8071273a7e",
+    "url": "/mock-critical/static/media/bag.b2ad852e.jpg"
+  },
+  {
+    "revision": "9fd4a4a88d5e5e2269fa82b751a99eb9",
+    "url": "/mock-critical/static/media/blog1.9fd4a4a8.jpg"
+  },
+  {
+    "revision": "ce018c9f368460a11c349dfd8fbf7f78",
+    "url": "/mock-critical/static/media/blog2.ce018c9f.jpg"
+  },
+  {
+    "revision": "bc5b2caa2a72f692035954abd4d6653c",
+    "url": "/mock-critical/static/media/blog3.bc5b2caa.jpg"
+  },
+  {
+    "revision": "1080f825d26bac629c5bfaace9955bbb",
+    "url": "/mock-critical/static/media/blog4.1080f825.jpg"
+  },
+  {
+    "revision": "aafd7a00ab725028359be6fb1bee9a9a",
+    "url": "/mock-critical/static/media/blog5.aafd7a00.jpg"
+  },
+  {
+    "revision": "a19ce2ded2e81a45908cc1e016d3d195",
+    "url": "/mock-critical/static/media/blog6.a19ce2de.jpg"
+  },
+  {
+    "revision": "10c621cb3eb6ac61f2b0f49b730d7de5",
+    "url": "/mock-critical/static/media/hood.10c621cb.jpg"
+  },
+  {
+    "revision": "5d60733ad1c9a4081817b300ee744e05",
+    "url": "/mock-critical/static/media/hoodie.5d60733a.jpg"
+  },
+  {
+    "revision": "d71c98b20a9224c638914743da600070",
+    "url": "/mock-critical/static/media/hoodie2.d71c98b2.jpg"
+  },
+  {
+    "revision": "fa51045c17e227bd917bc2304b8d179f",
+    "url": "/mock-critical/static/media/jacket.fa51045c.jpg"
+  },
+  {
+    "revision": "8fbdc263ed7cbbfad9a0be8e0f688f24",
+    "url": "/mock-critical/static/media/shirt.8fbdc263.jpg"
+  },
+  {
+    "revision": "a497687f3caddee52d0201fd94832b1a",
+    "url": "/mock-critical/static/media/shorts.a497687f.jpg"
+  },
+  {
+    "revision": "bfb2d099b6f67cef3a170280cc12ce7d",
+    "url": "/mock-critical/static/media/swimsuit.bfb2d099.jpg"
+  }
+]);
